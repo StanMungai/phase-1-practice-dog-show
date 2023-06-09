dab384727ebf007296ed9b4943162d1c5d46e4d4
StanMungai <stanfordkangethe@outlook.com>
2023-06-09 17:11:50 +0300

    first commit'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const tableBody = document.getElementById('table-body')
 
+    fetch('http://localhost:3000/dogs')
+      .then( res => res.json() )
+      .then( dogs => {
+        
+        dogs.forEach(dog => {
+            const row = document.createElement('tr')
+            const name = document.createElement('td')
+            const breed = document.createElement('td')
+            const sex = document.createElement('td')
+            const editDog = document.createElement('td')
+            const btn = document.createElement('btn')
+            btn.textContent = 'Edit Dog'
+
+            name.textContent = dog.name
+            breed.textContent = dog.breed
+            sex.textContent = dog.sex
+            editDog.appendChild(btn)
+
+            row.append(name, breed, sex, editDog)
+            tableBody.appendChild(row)
+        });
+      })
 })
+
+const createData = () => {
+    document.createElement('td')
+}
