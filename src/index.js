3e1479e009cfb0ee7e0a1a1ef56e13ee1b8a7e97
stanford kangethe <stanfordkangethe16@gmail.com>
2023-06-10 13:05:02 +0300

    To continue in patch

--- src/index.js
+++ src/index.js
@@ -1,30 +1,67 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const tableBody = document.getElementById('table-body')
+  const tableBody = document.getElementById('table-body')
 
-    fetch('http://localhost:3000/dogs')
-      .then( res => res.json() )
-      .then( dogs => {
-        
-        dogs.forEach(dog => {
-            const row = document.createElement('tr')
-            const name = document.createElement('td')
-            const breed = document.createElement('td')
-            const sex = document.createElement('td')
-            const editDog = document.createElement('td')
-            const btn = document.createElement('btn')
-            btn.textContent = 'Edit Dog'
+  fetch('http://localhost:3000/dogs') 
+    .then( res => res.json() )
+    .then( dogs => {
+      
+      dogs.forEach(dog => {
+        const inputForm = document.getElementById('dog-form')
+        const row = document.createElement('tr')
+        const name = document.createElement('td')
+        const breed = document.createElement('td')
+        const sex = document.createElement('td')
+        const editDog = document.createElement('td')
+        const btn = document.createElement('button')
+        btn.textContent = 'Edit Dog'
 
-            name.textContent = dog.name
-            breed.textContent = dog.breed
-            sex.textContent = dog.sex
-            editDog.appendChild(btn)
+        name.textContent = dog.name
+        breed.textContent = dog.breed
+        sex.textContent = dog.sex
+        editDog.appendChild(btn)
 
-            row.append(name, breed, sex, editDog)
-            tableBody.appendChild(row)
-        });
-      })
+        btn.addEventListener('click', () => {
+          inputForm.name.value = dog.name
+          inputForm.breed.value = dog.breed
+          inputForm.sex.value = dog.sex
+        })
+
+        inputForm.addEventListener('submit', (e) => {
+          e.preventDefault()
+          
+          fetch(`http://localhost:3000/dogs/${dog.id}`, {
+            method: 'PATCH',
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json"
+            },
+            body: JSON.stringify({
+              name: inputForm.name.value,
+              breed: inputForm.breed.value,
+              sex: inputForm.sex.value,
+            }) 
+          })
+            .then( res => res.json() )
+            .then( newDog => console.log(newDog))
+        })
+
+        row.append(name, breed, sex, editDog)
+        tableBody.appendChild(row)
+      });
+    })
 })
 
-const createData = () => {
-    document.createElement('td')
+const handleSubmit = (e) => {
+  e.preventDefault()
+  
+  fetch(`http://localhost:3000/dogs/${dog.id}`, {
+    method: 'PATCH',
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify() 
+  })
+    .then( res => res.json() )
+    .then( newDog => console.log(newDog))
 }
